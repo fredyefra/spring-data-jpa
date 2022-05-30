53d5d3f9f1567a22eeea5828d1bc18c46770d82a
fredyefra <fredweb3@gmail.com>
2022-05-29 23:40:09 -0300

    Bug issue list phones.

--- src/main/java/br/com/spring/jpa/util/DataService.java
+++ src/main/java/br/com/spring/jpa/util/DataService.java
@@ -3,6 +3,7 @@ package br.com.spring.jpa.util;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,32 +42,34 @@ public class DataService {
 		
 		Phone home = new Phone(null,"1111-1111" ,PhoneEnum.HOME);
 		Phone cell = new Phone(null,"2222-2222" ,PhoneEnum.CELL);
-		Phone job = new Phone(null,"2222-2222" ,PhoneEnum.JOB);
+		Phone job = new Phone(null,"3333-3333" ,PhoneEnum.JOB);
+		
+		Phone homec2 = new Phone(null,"4444-4444" ,PhoneEnum.HOME);
 		
 		//Collection<Phone> phones1 = Arrays.asList(home, celular);
 		
 		Customer c1 = new Customer();
 		c1.setFirstName("Robert C. Martin");
 		c1.setLastName("Martin");
-		c1.getPhones().addAll(List.of(home,cell));
+		c1.getPhones().addAll(List.of(home,cell,job));
 		
 		
 		Customer c2 = new Customer();
 		c2.setFirstName("Robert C. Martin 123");
 		c2.setLastName("Martin 321");
-		c2.getPhones().addAll(List.of(home));
+		c2.getPhones().addAll(List.of(homec2));
 		//c2.setPhones(phones1);
 		
 		
-		Collection<Customer> clientes = new ArrayList<Customer>();
+		Collection<Customer> clientes = Collections.synchronizedList(new ArrayList<>());
 		
 		clientes.addAll(List.of(c1,c2));
 		
 		
-		//service.saveAll(clientes);
+		service.saveAll(clientes);
 		
 		
-		customerRepository.saveAll(clientes);
+		//customerRepository.saveAll(clientes);
 		
 		//Customer c3 = new Customer(null, "James Gosling", "Gosling",a3,   null);
 		//Customer c4 = new Customer(null, "Erich Gamma", "Gamma",a4, null);		
