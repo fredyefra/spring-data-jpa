5696a996083e8a1423fa95fc63d347ab78045d88
fredyefra <fredweb3@gmail.com>
2022-06-14 23:51:27 -0300

    Add Swagger in project.

---
+++ src/main/java/br/com/spring/jpa/util/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package br.com.spring.jpa.util;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	public Docket api() {
+		
+	}
+	
+}
